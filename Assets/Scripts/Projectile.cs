077a32e7b9e777af645c6981c132690add380c17
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-07 02:41:58 +0000

    Nov 6, 2022, 9:41 PM

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
 
-    void awake()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
-
 
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
     }
 
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Projectile Collision with " + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
         Destroy(gameObject);
     }
 }
