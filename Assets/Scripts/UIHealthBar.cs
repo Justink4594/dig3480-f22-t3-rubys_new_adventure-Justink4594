43c23bd88f7e8e53119a2ab23abb7170ebdd8f42
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-08 07:24:59 +0000

    Nov 8, 2022, 2:24 AM

--- Assets/Scripts/UIHealthBar.cs
+++ Assets/Scripts/UIHealthBar.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHealthBar : MonoBehaviour
 {
+    public static UIHealthBar instance { get; private set; }
+
     public Image mask;
     float originalSize;
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
