968ad360a425a72c2e37976523ce7bd4da559af2
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-09 04:21:41 +0000

    Nov 8, 2022, 11:21 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -35,6 +35,10 @@ public class RubyController : MonoBehaviour
 
         audioSource = GetComponent<AudioSource>();
     }
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
 
     // Update is called once per frame
     void Update()
