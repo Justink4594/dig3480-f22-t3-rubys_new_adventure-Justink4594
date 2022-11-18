da26f673db0d722e2ffcc6537e8e1c4406400db9
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-18 07:58:50 +0000

    Nov 18, 2022, 2:58 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -31,6 +31,7 @@ public class RubyController : MonoBehaviour
 
     public ParticleSystem HealthIncrease;
     public ParticleSystem HealthDecrease;
+
     // Start is called before the first frame update
     void Start()
     {
