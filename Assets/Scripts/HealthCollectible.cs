addfc2a9adb6c44444298a5e50393991caca9ec5
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-20 06:09:55 +0000

    Nov 20, 2022, 1:09 AM

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
     public AudioClip collectedClip;
-    public 
+    public ParticleSystem HealthIncrease;
 
 
     void OnTriggerEnter2D(Collider2D other)
