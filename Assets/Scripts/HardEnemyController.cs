a71a41515af8e9a4dc0b45853c4e04463192486c
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-20 06:40:38 +0000

    Nov 20, 2022, 1:40 AM

--- Assets/Scripts/HardEnemyController.cs
+++ Assets/Scripts/HardEnemyController.cs
@@ -27,6 +27,65 @@ public class HardEnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!broken)
+        {
+            return;
+        }
+
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+            void FixedUpdate()
+        {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+            if (!broken)
+            {
+            return;
+            }
+
+            Vector2 position = rigidbody2D.position;
+
+            if (vertical)
+            {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+            }
+            else
+            {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+            }
+
+            rigidbody2D.MovePosition(position);
+    }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+        }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 }
+
+
