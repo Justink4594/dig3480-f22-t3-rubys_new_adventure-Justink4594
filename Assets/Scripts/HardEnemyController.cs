1f96f1e5ac3fefaa320620d7d099e9a10dc1f1f7
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-20 06:40:58 +0000

    Nov 20, 2022, 1:40 AM

--- Assets/Scripts/HardEnemyController.cs
+++ Assets/Scripts/HardEnemyController.cs
@@ -40,41 +40,41 @@ public class HardEnemyController : MonoBehaviour
             timer = changeTime;
         }
     }
-            void FixedUpdate()
-        {
+    void FixedUpdate()
+    {
         //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
-            if (!broken)
-            {
+        if (!broken)
+        {
             return;
-            }
+        }
 
-            Vector2 position = rigidbody2D.position;
+        Vector2 position = rigidbody2D.position;
 
-            if (vertical)
-            {
+        if (vertical)
+        {
             position.y = position.y + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", 0);
             animator.SetFloat("Move Y", direction);
-            }
-            else
-            {
+        }
+        else
+        {
             position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
-            }
+        }
 
-            rigidbody2D.MovePosition(position);
+        rigidbody2D.MovePosition(position);
     }
 
-        void OnCollisionEnter2D(Collision2D other)
-        {
+    void OnCollisionEnter2D(Collision2D other)
+    {
         RubyController player = other.gameObject.GetComponent<RubyController>();
 
         if (player != null)
         {
             player.ChangeHealth(-1);
         }
-        }
+    }
 
     //Public because we want to call it from elsewhere like the projectile script
     public void Fix()
