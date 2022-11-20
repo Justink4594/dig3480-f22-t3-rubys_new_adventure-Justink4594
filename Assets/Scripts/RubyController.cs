3aa19c99299b59e241cd0de111a7d57ef728bb08
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-20 06:08:07 +0000

    Nov 20, 2022, 1:08 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -98,7 +98,7 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
-        if (amount )
+        if (amount < 0)
         {
             if (isInvincible)
                 return;
@@ -110,7 +110,7 @@ public class RubyController : MonoBehaviour
             PlaySound(hitSound);
         }
 
-        if (amount )
+        if (amount < 0)
         {
             GameObject projectileObject = Instantiate(HealthIncrease, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
         }
