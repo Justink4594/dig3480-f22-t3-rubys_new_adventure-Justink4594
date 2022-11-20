bd1055c8a962d3a1bdbb69373fe3fb8e53f7e002
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-20 06:33:29 +0000

    Nov 20, 2022, 1:33 AM

--- Assets/Scripts/HardEnemyController.cs
+++ Assets/Scripts/HardEnemyController.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class HardEnemyController : MonoBehaviour
 {
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    public ParticleSystem smokeEffect;
+
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    bool broken = true;
+
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
