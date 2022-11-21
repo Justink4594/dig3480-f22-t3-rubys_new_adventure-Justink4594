44cd13a0f0731665455277f4db3ce86e57fa1ff0
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-21 07:17:41 +0000

    Nov 21, 2022, 2:17 AM

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    public AudioClip
+    public AudioClip Background;
+    public AudioClip Victory;
+    public AudioClip Defeat;
+    public AudioSource musicSource;
     // Start is called before the first frame update
     void Start()
     {
