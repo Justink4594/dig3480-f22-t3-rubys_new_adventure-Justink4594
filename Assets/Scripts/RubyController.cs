a7274817301d7d2ad6196e9129a255dbfa5d54ac
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-21 06:09:24 +0000

    Nov 21, 2022, 1:09 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -172,6 +172,6 @@ public class RubyController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Robots Fixed:" + count.ToString();
+        countText.text = "Robots Fixed:" + count;
     }
 }
