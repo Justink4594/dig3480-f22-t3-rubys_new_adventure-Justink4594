bf928e02e2e50c31aa6f8f6318f4211c427a418b
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-21 03:12:16 +0000

    Nov 20, 2022, 10:12 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,6 +16,8 @@ public class EnemyController : MonoBehaviour
 
     Animator animator;
 
+    private RubyController rubyController;
+
     // Start is called before the first frame update
     void Start()
     {
