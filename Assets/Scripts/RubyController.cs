e9dd2a8325262f0926b4d55c3275693df336fb10
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-21 04:31:20 +0000

    Nov 20, 2022, 11:31 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -137,7 +137,6 @@ public class RubyController : MonoBehaviour
             gameOver = true;
             gameoverTextObject.SetActive(true);
             speed = 0;
-            audioSource = false;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
