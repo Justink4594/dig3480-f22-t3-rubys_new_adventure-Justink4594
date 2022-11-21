197b8fdeb507be7e16621449e9a0d611fb98f448
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-21 04:04:30 +0000

    Nov 20, 2022, 11:04 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -16,6 +16,7 @@ public class RubyController : MonoBehaviour
     float invincibleTimer;
 
     AudioSource audioSource;
+    public GameObject BackgroundMusic;
     public GameObject projectilePrefab;
     public AudioClip throwSound;
     public AudioClip hitSound;
