82f4dc982ee6502ce725a71af96c56c6d142e204
Justink4594-main <justink4594@knights.ucf.edu>
2022-11-22 21:31:06 +0000

    Nov 22, 2022, 4:31 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -94,12 +94,13 @@ public class RubyController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
             if (hit.collider != null)
             {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
                 if (RFixed == 6)
                 {
                     SceneManager.LoadScene(Stage2);
                 }
-                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
-                if (character != null)
+
+                else if (character != null)
                 {
                     character.DisplayDialog();
                 }
